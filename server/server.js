449cb544cf971b7d6ea4f321161bf8604f7b6a14
RamirezKaren-FS <kjramirez1@student.fullsail.edu>
2024-05-16 17:51:46 -0500

    configured the .env and adjusted location

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT,() => {
     console.log(`Server is running ${process.env.NODE_ENV} on port ${PORT}`);
